d4327bd31c118fb486e24ac38488360a27e19cc0
Marcos Floriano <marcos.floriano@sptech.school>
2024-10-07 11:44:00 -0300

    criei a conexão com o back

---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080', // URL base da sua API
+});
+
+// Configurar interceptores se necessário
+api.interceptors.request.use(
+    (config) => {
+        const token = sessionStorage.getItem('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+export default api;
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -3,12 +3,32 @@ import Card from '../Card/Card';
 import Container from '../Container/Container';
 import Input from '../Input/Input';
 import Botao from '../Botao/Botao';
+import axios from 'axios';
 
 const LoginForm = () => {
+
+  const login = (event) => {
+
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+  
+    axios.post('http://localhost:8080/login', {
+      email: email,
+      senha: password
+    }).then((response) => {
+      sessionStorage.setItem('token', response.data.tokenJWT);
+    }
+    ).catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
       <Container>
         <Card>
-          <form>
+          <form onSubmit={login}>
             <Input name="email" type="email" label="Endereço de e-mail" placeholder="Digite seu e-mail" />
             <Input name="password" type="password" label="Senha" placeholder="Digite sua senha" />
             <Botao label="Entrar" className="btn-primary" />
