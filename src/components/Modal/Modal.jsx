553b7f3caa7343323a60828114d6a3d73f0eb0e9
Patrick Velasques <pettyvelasques@hotmail.com>
2024-10-19 21:08:55 -0300

    Finalizado modal de marcação de consultas

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = ({ show = true, onClose, title, content }) => {
         <div className={`${styles.modalContent} p-4 text-align-center`}>
           <div className={`${styles.modalHeader} px-0 pt-0`}>
             <h5 className={`${styles.modalTitle} col-md-9`}>{title}</h5>
-            <Botao label="X" className="col-md-3 btn-outline-primary text-align-center" onClick={onClose} style={{minWidth: '100%'}} />s
+            <Botao label="X" className="col-md-3 btn-outline-primary text-align-center" onClick={onClose} style={{minWidth: '100%'}} />
           </div>
           <div className={`${styles.modalBody} p-0`}>
             {content}
