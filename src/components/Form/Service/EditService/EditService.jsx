b94a36cb7c193a2f9fc5b6aa9ddf7dbc93e7f26d
LucasPagli <lucas.dsilva@sptech.school>
2025-02-10 11:06:49 -0300

    refactor: Adicionado filtro de categoria e outras correcoes

--- src/components/Form/Service/EditService/EditService.jsx
+++ src/components/Form/Service/EditService/EditService.jsx
@@ -116,18 +116,26 @@ const EditService = ({ serviceData, display, close }) => {
                     Preço*
                   </label>
                   <input
-                    type="number"
+                    type="text"
                     className="form-control"
                     id="servicePrice"
                     placeholder="Preço do serviço"
                     disabled={disabled}
                     value={serviceEdit.preco}
-                    onChange={(e) =>
-                      setServiceEdit({
-                        ...serviceEdit,
-                        preco: parseFloat(e.target.value),
-                      })
-                    }
+                    onChange={(e) => {
+                      const value = e.target.value.replace(",", "."); // Substitui vírgula por ponto
+                      if (!isNaN(value) || value === "") {
+                        setServiceEdit({ ...serviceEdit, preco: value });
+                      }
+                    }}
+                    onBlur={() => {
+                      if (serviceEdit.preco !== "" && !isNaN(serviceEdit.preco)) {
+                        setServiceEdit({
+                          ...serviceEdit,
+                          preco: parseFloat(serviceEdit.preco).toFixed(2),
+                        });
+                      }
+                    }}
                   />
                 </div>
                 <div className="col-md-6 mb-3">
@@ -158,8 +166,12 @@ const EditService = ({ serviceData, display, close }) => {
                     id="serviceDescription"
                     rows="3"
                     placeholder="Descrição do serviço"
+                    required={"true"}
                     disabled={disabled}
-                    required
+                    value={serviceEdit.descricao}
+                    onChange={(e) =>
+                      setServiceEdit({ ...serviceEdit, descricao: e.target.value })
+                    }
                   ></textarea>
                 </div>
                 <div className={style["lineButton"]}>
@@ -209,7 +221,7 @@ const EditService = ({ serviceData, display, close }) => {
     const dtoServicoAtualizado = {
       nome: serviceEdit?.nome,
       descricao: serviceEdit?.descricao,
-      preco: serviceEdit?.preco,
+      preco: parseFloat(serviceEdit?.preco),
       duracaoMinutos: serviceEdit?.duracaoMinutos,
       categoria: serviceEdit?.categoria,
     };
